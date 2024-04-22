df2eed8798e6694cc95f4ca53b4239757becf412
Micheal Butler <47530227+MichealEButler@users.noreply.github.com>
2024-04-22 16:22:05 +0100

    GIS added

--- _pages/maps.md
+++ _pages/maps.md
@@ -4,3 +4,5 @@ permalink: /maps/
 layout: posts
 author_profile: true
 ---
+
+<iframe src="assets/maps/map.html" height="700" width="700"></iframe>
---
+++ assets/maps/layers/layers.js
@@ -0,0 +1,95 @@
+var wms_layers = [];
+
+
+        var lyr_BingMapsSatelliteImagery_0 = new ol.layer.Tile({
+            'title': 'Bing Maps Satellite Imagery',
+            //'type': 'base',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+    attributions: ' ',
+                url: 'https://t0.tiles.virtualearth.net/tiles/a{q}.jpeg?g=685&mkt=en-us&n=z'
+            })
+        });
+var lyr_DEM_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "DEM",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/DEM_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-495530.346777, 7703554.704588, -288338.519184, 7831727.440007]
+                            })
+                        });
+var format_Rivers_2 = new ol.format.GeoJSON();
+var features_Rivers_2 = format_Rivers_2.readFeatures(json_Rivers_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Rivers_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Rivers_2.addFeatures(features_Rivers_2);
+var lyr_Rivers_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_Rivers_2, 
+                style: style_Rivers_2,
+                popuplayertitle: "Rivers",
+                interactive: true,
+                title: '<img src="styles/legend/Rivers_2.png" /> Rivers'
+            });
+var lyr_HPA2_3 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "HPA 2",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/HPA2_3.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-428743.160791, 7733762.471408, -372591.004949, 7786441.018531]
+                            })
+                        });
+var lyr_HPA1_4 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "HPA 1",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/HPA1_4.png",
+    attributions: ' ',
+                                projection: 'EPSG:3857',
+                                alwaysInRange: true,
+                                imageExtent: [-453466.561710, 7720297.487609, -341348.635958, 7814682.849570]
+                            })
+                        });
+var format_sites_5 = new ol.format.GeoJSON();
+var features_sites_5 = format_sites_5.readFeatures(json_sites_5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_sites_5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_sites_5.addFeatures(features_sites_5);
+var lyr_sites_5 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_sites_5, 
+                style: style_sites_5,
+                popuplayertitle: "sites",
+                interactive: true,
+                title: '<img src="styles/legend/sites_5.png" /> sites'
+            });
+
+lyr_BingMapsSatelliteImagery_0.setVisible(true);lyr_DEM_1.setVisible(true);lyr_Rivers_2.setVisible(true);lyr_HPA2_3.setVisible(true);lyr_HPA1_4.setVisible(true);lyr_sites_5.setVisible(true);
+var layersList = [lyr_BingMapsSatelliteImagery_0,lyr_DEM_1,lyr_Rivers_2,lyr_HPA2_3,lyr_HPA1_4,lyr_sites_5];
+lyr_Rivers_2.set('fieldAliases', {'name1': 'name1', 'identifier': 'identifier', 'startNode': 'startNode', 'endNode': 'endNode', 'form': 'form', 'flow': 'flow', 'fictitious': 'fictitious', 'length': 'length', 'name2': 'name2', });
+lyr_sites_5.set('fieldAliases', {'site': 'site', 'easting': 'easting', 'northing': 'northing', 'NGR': 'NGR', 'visible': 'visible', 'letter lab': 'letter lab', 'map_no': 'map_no', });
+lyr_Rivers_2.set('fieldImages', {'name1': 'TextEdit', 'identifier': 'TextEdit', 'startNode': 'TextEdit', 'endNode': 'TextEdit', 'form': 'TextEdit', 'flow': 'TextEdit', 'fictitious': 'TextEdit', 'length': 'Range', 'name2': 'TextEdit', });
+lyr_sites_5.set('fieldImages', {'site': 'TextEdit', 'easting': 'TextEdit', 'northing': 'TextEdit', 'NGR': 'TextEdit', 'visible': 'TextEdit', 'letter lab': 'TextEdit', 'map_no': 'TextEdit', });
+lyr_Rivers_2.set('fieldLabels', {'name1': 'no label', 'identifier': 'no label', 'startNode': 'no label', 'endNode': 'no label', 'form': 'no label', 'flow': 'no label', 'fictitious': 'no label', 'length': 'no label', 'name2': 'no label', });
+lyr_sites_5.set('fieldLabels', {'site': 'no label', 'easting': 'no label', 'northing': 'no label', 'NGR': 'no label', 'visible': 'no label', 'letter lab': 'no label', 'map_no': 'no label', });
+lyr_sites_5.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
